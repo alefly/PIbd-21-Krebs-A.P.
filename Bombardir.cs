06c8b067fd61204353b150f1137072b29332ceec
alefly <alyona.krebs@yandex.ru>
2017-12-19 00:42:22 +0400

    Add files via upload

---
+++ Bombardir.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+	public class Bombardir : Plane
+	{
+		private bool left;
+		private bool right;
+        private Color dopColor;
+
+
+        public Bombardir(int maxSpeed, int maxCountBomb, double weight, Color color, bool left, bool right, bool v, Color dopColor) : base(maxSpeed, maxCountBomb, weight, color, left, right)
+        {
+            this.left = left;
+            this.right = right;
+            this.dopColor = dopColor;
+        }
+
+        protected override void drawLightBombardir(Graphics g)
+		{
+            base.drawLightBombardir(g);
+ 
+            Pen pen = new Pen(dopColor);
+            Brush br = new SolidBrush(dopColor);
+            PointF[] pf1 = new PointF[5];
+
+            pf1[0] = new Point((int)startPosX + 20, (int)startPosY + 23 + 1 + 5);
+            pf1[1] = new Point((int)startPosX + 20 + 80, (int)startPosY + 23 + 1 + 5);
+            pf1[2] = new Point((int)startPosX + 20 + 80 + 25, (int)startPosY + 23 + 30 + 1 + 5);
+            pf1[3] = new Point((int)startPosX + 20 - 25, (int)startPosY + 23 + 30 + 1 + 5);
+            pf1[4] = new Point((int)startPosX + 20, (int)startPosY + 23 + 1 + 5);
+
+            g.FillPolygon(br, pf1);
+        }
+
+		public void setDopColor(Color color)
+		{
+			dopColor = color;
+		}
+	}
+}
---
+++ ITechnique.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+	public interface ITechnique
+	{
+		void moveBombardir(Graphics g);
+
+        void moveUpBombardir(Graphics g);
+
+        void moveBomb(Graphics g);
+
+		void setPosition(int x, int y);
+
+        void drawBombardir(Graphics gr);
+
+		void setMainColor(Color color);
+    }
+}
