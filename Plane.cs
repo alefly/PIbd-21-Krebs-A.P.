6972ac9c08a68449f8d3f93e883ffdbdcc383a5a
alefly <alyona.krebs@yandex.ru>
2017-12-19 00:13:54 +0400

    Add files via upload

---
+++ Plane.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_v2._0
+{
+    class UFO : Vehicle
+    {
+        public override int MaxSpeed
+        {
+            get
+            {
+                return base.MaxSpeed;
+            }
+            protected set
+            {
+                if (value > 0 && value < 300)
+                {
+                    base.MaxSpeed = value;
+                }
+                else
+                {
+                    base.MaxSpeed = 150;
+                }
+            }
+        }
+        public override int MaxCountPassengers
+        {
+            get
+            {
+                return base.MaxCountPassengers;
+            }
+            protected set
+            {
+                if (value > 0 && value < 5)
+                {
+                    base.MaxCountPassengers = value;
+                }
+                else
+                {
+                    base.MaxCountPassengers = 4;
+                }
+            }
+        }
+
+        public override double Weight
+        {
+            get
+            {
+                return base.Weight;
+            }
+            protected set
+            {
+                if (value > 500 && value < 1500)
+                {
+                    base.Weight = value;
+                }
+                else
+                {
+                    base.Weight = 1000;
+                }
+            }
+        }
+
+        public UFO(int maxSpeed, int maxCountPassenger, double weight, Color color)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.MaxCountPassengers = maxCountPassenger;
+            this.ColorBody = color;
+            this.Weight = weight;
+            this.countPassengers = 0;
+            Random rand = new Random();
+            startPosX = rand.Next(10, 200);
+            startPosY = rand.Next(10, 200);
+        }
+        public override void moveUFO(Graphics g)
+        {
+            startPosX += (MaxSpeed * 50 / (float)Weight) / (countPassengers == 0 ? 1 : countPassengers);
+            drawUFO(g);
+        }
+
+        public override void drawUFO(Graphics g)
+        {
+            drawSuperUFO(g);
+        }
+        protected virtual void drawSuperUFO(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            g.DrawEllipse(pen, startPosX + 20, startPosY - 10, 80, 30);
+            Brush br = new SolidBrush(ColorBody);
+            PointF[] pf = new PointF[7];
+            pf[0] = new Point((int)startPosX + 20, (int)startPosY);
+            pf[1] = new Point((int)startPosX + 100, (int)startPosY);
+            pf[5] = new Point((int)startPosX, (int)startPosY + 12);
+            pf[2] = new Point((int)startPosX + 120, (int)startPosY + 12);
+            pf[4] = new Point((int)startPosX + 20, (int)startPosY + 24);
+            pf[3] = new Point((int)startPosX + 100, (int)startPosY + 24);
+            pf[6] = new Point((int)startPosX + 20, (int)startPosY);
+            g.FillPolygon(br, pf);
+            g.DrawLines(pen, pf);
+            g.DrawLine(pen, pf[2], pf[5]);
+            
+        }
+
+
+    }
+}
---
+++ Technique.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace lab_v2._0
+{
+    public abstract class Vehicle : ITech
+    {
+        protected int weight;
+        protected int maxSpeed;
+        protected Color colorBody;
+        protected float startPosX;
+        protected float startPosY;
+        protected int countPassengers;
+        public virtual int MaxCountPassengers
+        {
+            protected set;
+            get;
+        }
+
+        public virtual int MaxSpeed
+        {
+            protected set;
+            get;
+        }
+
+        public Color ColorBody
+        {
+            protected set;
+            get;
+        }
+
+        public virtual double Weight
+        {
+            protected set;
+            get;
+        }
+
+
+        public abstract void moveUFO(Graphics g);
+
+        public abstract void drawUFO(Graphics g);
+
+        public void setPosition(int x, int y)
+        {
+            startPosX = x;
+            startPosY = y;
+        }
+
+        public void loadPassenger(int count)
+        {
+            if (countPassengers + count < MaxCountPassengers)
+            {
+                countPassengers += count;
+            }
+        }
+
+        public int getPassenger()
+        {
+            int count = countPassengers;
+            countPassengers = 0;
+            return count;
+        }
+
+    }
+}
