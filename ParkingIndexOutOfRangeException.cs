ca8cf9fb26548b9f9c2bb80be17cffb246cd57a6
alefly <alyona.krebs@yandex.ru>
2018-01-18 13:28:18 +0400

    Add files via upload

---
+++ ParkingIndexOutOfRangeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+    class ParkingIndexOutOfRangeException : Exception
+    {
+        public ParkingIndexOutOfRangeException() : base("На парковке нет машины на этом месте")
+        {
+        }
+    }
+}
