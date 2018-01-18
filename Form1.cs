072aac8d82886752206d744d3aa9bc5923754e12
alefly <alyona.krebs@yandex.ru>
2018-01-18 13:21:14 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        Parking parking;
+
+        FormSelectCar form;
+        private Logger log;
+        private Logger logEx;
+        StreamWriter w;
+
+        public Form1()
+        {
+            InitializeComponent();
+            log = LogManager.GetCurrentClassLogger();
+            w = File.AppendText("D://log.txt");
+            parking = new Parking(5);
+            for (int i = 1; i < 6; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + i);
+
+            }
+            listBoxLevels.SelectedIndex = parking.getCurrentLevel;
+            Draw();
+        }
+
+        private void Draw()
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking.Draw(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var plane = new Plane(100, 4, 1000, dialog.Color);
+                //int place = parking.PutPlaneInParking(plane);
+                //Draw();
+                //MessageBox.Show("Ваше место: " + place);
+                if (plane != null)
+                {
+                    try
+                    {
+                        int place = parking.PutPlaneInParking(plane);
+                        Draw();
+                        MessageBox.Show("Ваше место: " + place);
+                        log.Info("Занято место: " + place);
+                        
+                    }
+                    catch (ParkingOverflowException ex)
+                    {
+                        log.Error("Ошибка переполнения");
+                        MessageBox.Show(ex.Message, "Ошибка переполнения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (ParkingIndexOutOfRangeException ex)
+                    {
+                        log.Error("Общая ошибка");
+                        MessageBox.Show(ex.Message, "Общая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var plane = new Bombardir(100, 4, 1000, dialog.Color, true, true, true, dialogDop.Color);
+                    try
+                    {
+                        int place = parking.PutPlaneInParking(plane);
+                        Draw();
+                        MessageBox.Show("Ваше место: " + place);
+                        log.Info("Занято место: " + place);
+                        
+                    }
+                    catch (ParkingOverflowException ex)
+                    {
+                        log.Error("Ошибка переполнения");
+                        MessageBox.Show(ex.Message, "Ошибка переполнения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (ParkingIndexOutOfRangeException ex)
+                    {
+                        log.Error("Общая ошибка");
+                        MessageBox.Show(ex.Message, "Общая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                try
+                {
+                    var plane = parking.GetPlaneInParking(Convert.ToInt32(maskedTextBox1.Text));
+
+                    Bitmap bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                    Graphics gr = Graphics.FromImage(bmp);
+                    plane.setPosition(3, 5);
+                    plane.drawBombardir(gr);
+                    pictureBox2.Image = bmp;
+
+                    log.Info("Объект изъят с места " + Convert.ToInt32(maskedTextBox1.Text));
+                    
+                    Draw();
+
+                }
+
+                catch (ParkingIndexOutOfRangeException ex)
+                {
+
+                    log.Error("Объект не найден");
+                    MessageBox.Show(ex.Message, "Ошибка переполнения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    log.Error("Общая ошибка");
+                    MessageBox.Show(ex.Message, "Общая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            parking.LevelDown();
+            listBoxLevels.SelectedIndex = parking.getCurrentLevel;
+            log.Info("Переход на уровень ниже. Текущий уровень: " + parking.getCurrentLevel);
+           
+            Draw();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            parking.LevelUp();
+            listBoxLevels.SelectedIndex = parking.getCurrentLevel;
+            log.Info("Переход на уровень выше. Текущий уровень: " + parking.getCurrentLevel);
+           
+            Draw();
+        }
+
+        private void buttonSetPlane_Click(object sender, EventArgs e)
+        {
+            form = new FormSelectCar();
+            form.ShowDialog();
+            var plane = form.getPlane;
+            if (plane != null)
+            {
+                try
+                {
+                    int place = parking.PutPlaneInParking(plane);
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                    log.Info("Занято место: " + place);
+                   
+                }
+                catch (ParkingOverflowException ex)
+                {
+                    log.Error("Ошибка переполнения");
+                    MessageBox.Show(ex.Message, "Ошибка переполнения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (ParkingIndexOutOfRangeException ex)
+                {
+                    log.Error("Общая ошибка");
+                    MessageBox.Show(ex.Message, "Общая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.SaveData(saveFileDialog1.FileName))
+                {
+                    MessageBox.Show("Сохранение прошло успешно", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    log.Info("Сохранение выполнено");
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранилось", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    log.Info("Сохранение не выполнено");
+                }
+            }
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.LoadData(openFileDialog1.FileName))
+                {
+                    MessageBox.Show("Загрузили", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    log.Info("Загрузка выполнена");
+                }
+                else
+                {
+                    MessageBox.Show("Незагрузили", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    log.Info("Загрузка не выполнена");
+                }
+                Draw();
+            }
+
+        }
+
+        
+        
+    }
+
+
+    
+}
+
+
---
+++ Plane.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+    public class Plane : Technique
+    {
+        private bool left;
+        private bool right;
+        public Color dopColor;
+
+        public string CN() {
+            return ";" + dopColor.Name;
+        }
+        public override int MaxSpeed
+        {
+            get
+            {
+                return base.MaxSpeed;
+            }
+            protected set
+            {
+                if (value > 0 && value < 1500)
+                {
+                    base.MaxSpeed = value;
+                }
+                else
+                {
+                    base.MaxSpeed = 1000;
+                }
+            }
+        }
+
+        public override int MaxCountBomb
+        {
+            get
+            {
+                return base.MaxCountBomb;
+            }
+            protected set
+            {
+                if (value > 0 && value < 5)
+                {
+                    base.MaxCountBomb = value;
+                }
+                else
+                {
+                    base.MaxCountBomb = 4;
+                }
+            }
+        }
+
+        public override double Weight
+        {
+            get
+            {
+                return base.Weight;
+            }
+            protected set
+            {
+                if (value > 500 && value < 1500)
+                {
+                    base.Weight = value;
+                }
+                else
+                {
+                    base.Weight = 1000;
+                }
+            }
+        }
+
+        public Plane(int maxSpeed, int maxCountBomb, double weight, Color color)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.MaxCountBomb = maxCountBomb;
+            this.ColorBody = color;
+            this.Weight = weight;
+            this.countBomb = 0;
+            Random rand = new Random();
+            startPosX = rand.Next(10, 200);
+            startPosY = rand.Next(10, 200);
+        }
+
+        public Plane(string info) {
+            string[] strs = info.Split(';');
+            if(strs.Length == 5) {
+                this.MaxSpeed = Convert.ToInt32(strs[0]);
+                this.MaxCountBomb = Convert.ToInt32(strs[1]);
+                this.Weight = Convert.ToInt32(strs[2]);
+                this.ColorBody = Color.FromName(strs[3]);
+               
+            }
+            this.countBomb = 0;
+            dopColor = Color.White;
+            Random rand = new Random();
+            startPosX = rand.Next(10, 200);
+            startPosY = rand.Next(10, 200);
+        }
+
+        public Plane(int maxSpeed, int maxCountBomb, double weight, Color color, bool left, bool right) : this(maxSpeed, maxCountBomb, weight, color)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override void moveBombardir(Graphics g)
+        {
+            startPosX += (MaxSpeed * 50 / (float)Weight) / (countBomb == 0 ? 1 : countBomb);
+            drawBombardir(g);
+        }
+
+        public override void moveUpBombardir(Graphics g)
+        {
+            startPosY -= (MaxSpeed * 50 / (float)Weight) / (countBomb == 0 ? 1 : countBomb);
+            drawBombardir(g);
+        }
+
+        public override void drawBombardir(Graphics g)
+        {
+            drawLightBombardir(g);
+        }
+
+        protected virtual void drawLightBombardir(Graphics g)
+        {
+           
+            Pen pen = new Pen(Color.Black);
+            g.DrawEllipse(pen, startPosX + 20, startPosY - 10 + 5, 80, 30);
+            Brush br = new SolidBrush(this.ColorBody);
+            PointF[] pf = new PointF[7];
+           
+            pf[0] = new Point((int)startPosX + 20, (int)startPosY + 5);
+            pf[1] = new Point((int)startPosX + 100, (int)startPosY + 5);
+            pf[5] = new Point((int)startPosX, (int)startPosY + 12 + 5);
+            pf[2] = new Point((int)startPosX + 120, (int)startPosY + 12 + 5);
+            pf[4] = new Point((int)startPosX + 20, (int)startPosY + 24 + 5);
+            pf[3] = new Point((int)startPosX + 100, (int)startPosY + 24 + 5);
+            pf[6] = new Point((int)startPosX + 20, (int)startPosY + 5);
+            g.FillPolygon(br, pf);
+            g.DrawLines(pen, pf);
+            g.DrawLine(pen, pf[2], pf[5]);
+
+            
+        }
+
+
+        public string getInfo()
+        {
+            return 199999999999 + ";" + MaxCountBomb + ";" + Weight + ";" + ColorBody.Name + ";" + dopColor.Name;
+        }
+        public override void moveBomb(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void giveBomb(Graphics g)
+        {
+            takeBomb(g);
+        }
+
+        protected virtual void takeBomb(Graphics g)
+        {
+
+        }
+
+    }
+
+}
