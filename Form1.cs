981f6909a6989fa4fdbb4483a6cf5bc81dae7ee9
alefly <alyona.krebs@yandex.ru>
2018-01-18 13:54:13 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        Parking parking;
+
+        public Form1()
+        {
+            InitializeComponent();
+            parking = new Parking(15);
+            Draw();
+        }
+        /// <summary>
+        /// Метод для отрисовки парковки
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            parking.Draw(gr);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+            private void buttonSetPlane_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var car = new Plane(100, 4, 1000, dialog.Color);
+                int place = parking.PutPlaneInParking(car);
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var car = new Bombardir(100, 4, 1000, dialog.Color, true, true, true, dialogDop.Color);
+                    int place = parking.PutPlaneInParking(car);
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+            }
+        }
+
+        private void buttonTakePlane_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                var car = parking.GetPlaneInParking(Convert.ToInt32(maskedTextBox1.Text));
+
+                //Bitmap bmp = new Bitmap(pictureBox2.Width, pictureBoxTakeCar.Height);
+                //Graphics gr = Graphics.FromImage(bmp);
+                //car.setPosition(5, 5);
+                //car.drawCar(gr);
+                //pictureBoxTakeCar.Image = bmp;
+                //Draw();
+            }
+        }
+
+    }
+}
+
+
---
+++ Parking.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+	public class Parking
+	{
+		//список с уровнями парковки
+		List<ClassArray<ITechnique>> parkingStages;
+		///сколько мест на каждом уровне
+		int countPlaces = 20;
+		//// ширина парковочного места
+		int placeSizeWidth = 200;
+		//// длина парковочного места
+		int placeSizeHeighth = 120;
+		///текущий уровень 
+		int currentLevel;
+		///получить текущий уровень
+		public int getCurrentLevel { get { return currentLevel; } }
+		public Parking(int countStages)
+		{
+			parkingStages = new List<ClassArray<ITechnique>>(countStages);
+			for( int i = 0; i<countStages; i++)
+			{
+				parkingStages.Add(new ClassArray<ITechnique>(countPlaces, null));
+			}
+		}
+
+		// перейти на уровень выше
+		public void LevelUp()
+		{
+			if (currentLevel + 1 < parkingStages.Count)
+			{
+				currentLevel++;
+			}
+		}
+
+		////перейти на уровень ниже
+		public void LevelDown()
+		{
+			if (currentLevel > 0)
+			{
+				currentLevel--;
+			}
+		}
+
+		// поставить машину на парковку
+
+		public int PutPlaneInParking(ITechnique plane)
+		{
+			return parkingStages[currentLevel] + plane;
+		}
+
+		//// забрать машину с парковки
+
+		public ITechnique GetPlaneInParking(int ticket)
+		{
+			return parkingStages[currentLevel] - ticket;
+		}
+
+		/////отрисовка 
+
+
+		public void Draw(Graphics g)
+		{
+			DrawMarking(g);
+			for (int i = 0; i < countPlaces; i++)
+			{
+				var plane = parkingStages[currentLevel][i];
+				if (plane != null)
+				{ ///если место не пустое
+					plane.setPosition(5 + i / 5 * 200 + 5, i % 5 * 120 + 15);
+					plane.drawBombardir(g);
+				}
+			}
+		}
+
+		/// отрисовка разметки парковки
+
+
+		private void DrawMarking(Graphics g)
+		{
+			Pen pen = new Pen(Color.Black, 3);
+			///границы парковки
+			g.DrawString("L" + (currentLevel + 1), new Font("Arial", 30), new SolidBrush(Color.Blue), (countPlaces / 5) * placeSizeWidth - 70, 420);
+
+			g.DrawRectangle(pen, 0, 0, (countPlaces / 5) * placeSizeWidth, 580);
+			for (int i = 0; i < countPlaces / 5; i++)
+			{
+				///отрисовываем, по 5 мест на линии
+				for (int j = 0; j < 6; ++j)
+				{/// линия разметки места
+					g.DrawLine(pen, i * placeSizeWidth, j * placeSizeHeighth, i * placeSizeWidth + 110, j * placeSizeHeighth);
+					if (j < 5)
+					{
+						g.DrawString((i * 5 + j + 1).ToString(), new Font("Arial", 30),
+						new SolidBrush(Color.Blue), i * placeSizeWidth + 30, j * placeSizeHeighth + 20);
+					}
+				}
+				g.DrawLine(pen, i * placeSizeWidth, 0, i * placeSizeWidth, 580);
+			}
+		}
+		}
+}
